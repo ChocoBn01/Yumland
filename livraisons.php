aa7b48ed20dc1960e5954faf895ea63c1d13a781
Nathan Evina <nathaneviname@gmail.com>
2026-03-12 12:25:16 +0100

    Update and rename livraisons.html to livraisons.php

--- livraisons.php
+++ livraisons.php
@@ -18,11 +18,11 @@
         </div>
         <nav>
             <ul>
-                <li><a href="index.html">Accueil</a></li>
-                <li><a href="menu.html">La Carte</a></li>
-                <li><a href="profil.html">Mon Profil</a></li>
-                <li><a href="commandes.html" class="active">Livraison</a></li>
-                <li><a href="index.html" class="btn">Déconnexion</a></li>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="menu.php">La Carte</a></li>
+                <li><a href="profil.php">Mon Profil</a></li>
+                <li><a href="commandes.php" class="active">Livraison</a></li>
+                <li><a href="index.php" class="btn">Déconnexion</a></li>
             </ul>
         </nav>
     </header>
