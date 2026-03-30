526e902132be6d5dab6fe09ad45d2584b3856e66
Nathan Evina <nathaneviname@gmail.com>
2026-03-30 10:46:50 +0200

    Refactor client cookie handling and functions

--- livraisons.php
+++ livraisons.php
@@ -1,96 +1,108 @@
 <?php
-if(!isset($_COOKIE["client"])){
-        header("Location: index.php");
-}
-$client = json_decode($_COOKIE["client"], true);
-$liste_client_data = file_get_contents("donnees/data.json");
-$liste_client = json_decode($liste_client_data, true);
-$commande_data = file_get_contents("donnees/commande.json");
-$commande = json_decode($commande_data, true);
-if($liste_client[$client['email']]['role']['bloque']==true){
-        setcookie("client", ", time()-3600);  
+    if(!isset($_COOKIE["client"])){
         header("Location: index.php");
     }
-function aff_num_cmd_ou_fidelite($num, $cmd_ou_fidelite){
-    if ($cmd_ou_fidelite == 1) {
-        if ($num < 10) {
-            echo "000" . $num;
-        } elseif ($num < 100) {
-            echo "00" . $num;
-        } elseif ($num < 1000) {
-            echo "0" . $num;
-        } else {
-            echo $num;
-        }
-    } else {
-        if ($num < 10) {
-            echo "0000000" . $num;
-        } elseif ($num < 100) {
-            echo "000000" . $num;
-        } elseif ($num < 10000) {
-            echo "00000" . $num;
-        } elseif ($num < 100000) {
-            echo "0000" . $num;
-        } elseif ($num < 1000000) {
-            echo "000" . $num;
-        } elseif ($num < 10000000) {
-            echo "00" . $num;
-        } elseif ($num < 100000000) {
-            echo "0" . $num;
-        } else {
-            echo $num;
-        }
+    $client = json_decode($_COOKIE["client"], true);
+    $liste_client_data = file_get_contents("donnees/data.json");
+    $liste_client = json_decode($liste_client_data, true);
+    $commande_data = file_get_contents("donnees/commande.json");
+    $commande = json_decode($commande_data, true);
+    if($liste_client[$client['email']]['role']['bloque']==true ){
+        setcookie("client", json_encode($data[$mail]), time()-3600);  
+        header("Location: index.php");
     }
-}
-$cmd_ou_pas = false;
-$temps=0;
-if (!empty($commande)) {
-    foreach ($commande as $id_cmd => $detail) {
-        if ($detail["etat"]["cuisinee"] == true &&$detail["livreur"] == $client["email"]) {
-            $cmd_ou_pas = true;
-            if($detail['temps']>=$temps){
-                $cmd=$detail;
-                $temps=$detail['temps'];
+    if($liste_client[$client['email']]['role']['livreur']==false ){
+        header("Location: profil.php");
+    }
+    function aff_num_cmd_ou_fidelite($num, $cmd_ou_fidelite){
+        if ($cmd_ou_fidelite == 1) {
+            if ($num < 10) {
+                echo "000" . $num;
+            } elseif ($num < 100) {
+                echo "00" . $num;
+            } elseif ($num < 1000) {
+                echo "0" . $num;
+            } 
+            else {
+                echo $num;
+            }
+        } 
+        else {
+            if ($num < 10) {
+                echo "0000000" . $num;
+            } 
+            elseif ($num < 100) {
+                echo "000000" . $num;
+            } 
+            elseif ($num < 10000) {
+                echo "00000" . $num;
+            } 
+            elseif ($num < 100000) {
+                echo "0000" . $num;
+            } 
+            elseif ($num < 1000000) {
+                echo "000" . $num;
+            } 
+            elseif ($num < 10000000) {
+                echo "00" . $num;
+            } 
+            elseif ($num < 100000000) {
+                echo "0" . $num;
+            } 
+            else {
+                echo $num;
             }
         }
     }
-}
-function aff_temps($num)
-{
-    if ($num < 10) {
-        return "0" . $num;
-    } else {
-        return $num;
+    $cmd_ou_pas = false;
+    $temps=0;
+    if (!empty($commande)) {
+        foreach ($commande as $id_cmd => $detail) {
+            if ($detail["etat"]["cuisinee"] == true &&$detail["livreur"] == $client["email"]) {
+                $cmd_ou_pas = true;
+                if($detail['temps']>=$temps){
+                    $cmd=$detail;
+                    $temps=$detail['temps'];
+                }
+            }
+        }
     }
-}
-function aff_num_cmd_sans_echo($num){
-    if($num<10){
-        return "000".$num;
+    function aff_temps($num){
+        if ($num < 10) {
+            return "0" . $num;
+        } 
+        else {
+            return $num;
+        }
     }
-    else if($num<100){
-        return "00".$num;
+    function aff_num_cmd_sans_echo($num){
+        if($num<10){
+            return "000".$num;
+        }
+        else if($num<100){
+            return "00".$num;
+        }
+        else if($num<1000){
+            return "0".$num;
+        }
+        else{
+            return $num;
+        }
     }
-    else if($num<1000){
-        return "0".$num;
+    if(isset($_REQUEST['button'])){
+        $file_data=file_get_contents("donnees/commande_passe.json");
+        $file=json_decode($file_data, true);
+        $mail=$cmd['mail'];
+        $new_tab=array('num'=>$cmd['num'], 'date'=>$cmd['date'], 'total'=>$cmd['total'], 'plats'=>$cmd['plats']);
+        $file[$mail][aff_num_cmd_sans_echo($cmd['num'])]=$new_tab;
+        $liste_client[$mail]['point_fidelite']+=intval($cmd['total']);
+        file_put_contents("donnees/data.json", json_encode($liste_client, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+        unset($commande[aff_num_cmd_sans_echo($cmd['num'])]);
+        file_put_contents("donnees/commande_passe.json", json_encode($file, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+        file_put_contents("donnees/commande.json", json_encode($commande, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+        header("Location: livraisons.php");
+        exit;
     }
-    else{
-        return $num;
-    }
-}
-if(isset($_REQUEST['button'])){
-    $file_data=file_get_contents("donnees/commande_passe.json");
-    $file=json_decode($file_data, true);
-    $mail=$cmd['mail'];
-    $new_tab=array('num'=>$cmd['num'], 'date'=>$cmd['date'], 'total'=>$cmd['total'], 'plats'=>$cmd['plats']);
-    $file[$mail][aff_num_cmd_sans_echo($cmd['num'])]=$new_tab;
-    $liste_client[$mail]['point_fidelite']+=intval($cmd['total']);
-    file_put_contents("donnees/data.json", json_encode($liste_client, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-    unset($commande[aff_num_cmd_sans_echo($cmd['num'])]);
-    file_put_contents("donnees/commande_passe.json", json_encode($file, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-    file_put_contents("donnees/commande.json", json_encode($commande, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-    header("Location: livraisons.php");
-    exit;
-}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
