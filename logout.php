5af8d1e47f7fd5aedf8bf756e0cd88185d2a55ed
Nathan Evina <nathaneviname@gmail.com>
2026-04-01 10:28:33 +0200

    Enhance logout functionality with cart handling
    Refactor logout logic to handle client data and cart management.

--- logout.php
+++ logout.php
@@ -1,6 +1,24 @@
-<?php
+<?php 
     if(!isset($_COOKIE["client"])){
         header("Location: index.php");
+        exit;
+    }
+    $client=json_decode($_COOKIE["client"], true);
+    $file=file_get_contents("donnees/data.json");
+    $data=json_decode($file, true);
+    $mail=$client['email'];   
+    $commande_data =file_get_contents("donnees/panier_$mail.json", true);
+    $commande = json_decode($commande_data, true); 
+    if(file_exists("donnees/panier.json")){
+        $panier=json_decode(file_get_contents("donnees/panier.json"), true);
+        $panier[$mail]=$commande;
+    }
+    else{
+        $panier[$mail]=$commande;
+    }
+    file_put_contents("donnees/panier.json", json_encode($panier, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    if(file_exists("donnees/panier_$mail.json")){
+        unlink("donnees/panier_$mail.json");
     }
     setcookie("client", json_encode($data[$mail]), time()-3600);  
     header("Location: index.php");
