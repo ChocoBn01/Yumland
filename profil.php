164a2f8ca36d94619c78774fc03406fc721627a4
Nathan Evina <nathaneviname@gmail.com>
2026-04-01 12:08:58 +0200

    Add exit after header redirect for client check
    Added exit statement after redirecting to index.php.

--- profil.php
+++ profil.php
@@ -1,6 +1,7 @@
 <?php 
     if(!isset($_COOKIE["client"])){
         header("Location: index.php");
+        exit;
     }
     $client=json_decode($_COOKIE["client"], true);   
     $commande_data =file_get_contents("donnees/commande_passe.json");
