d9f3af77196aa5e438e13ecf1902432cbec6a124
Nathan Evina <nathaneviname@gmail.com>
2026-04-05 21:09:58 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,236 +1,240 @@
 <?php 
     if(!isset($_COOKIE["client"])){
         header("Location: index.php");
-        exit;
     }
-    if(isset($_COOKIE["admin"])){
-        $client=json_decode($_COOKIE["admin"], true); 
-    }
-    else{
-        $client=json_decode($_COOKIE["client"], true);
-    }
-    $commande_data =file_get_contents("donnees/commande_passe.json");
-    $commande = json_decode($commande_data, true); 
+    $client=json_decode($_COOKIE["client"], true);   
     $file=file_get_contents("donnees/data.json");
     $data=json_decode($file, true);
-    setcookie("client", json_encode($data[$client['email']]), time()-3600);  
-    setcookie("client", json_encode($data[$client['email']]), time()+3600);
-    $client=json_decode($_COOKIE["client"], true);
     if($data[$client['email']]['role']['bloque']==true){
-        setcookie("client", json_encode($data[$mail]), time()-3600);  
+        setcookie("client", "", time()-3600);  
         header("Location: index.php");
+    }   
+    if($data[$client['email']]['role']['admin']==false ){
+        header("Location: profil.php");
     }
-    $mail=$client['email'];   
-    if(!file_exists("donnees/panier_$mail.json")){
-        $panier=array("total"=>0, "reduction"=>false);
-        $panier_data="donnees/panier_$mail.json";
-        file_put_contents($panier_data, json_encode($panier, JSON_PRETTY_PRINT));
+    function aff_role($client){
+        if($client['role']['bloque']==true){
+            echo "BLOQUE";
+        }
+        else if($client['role']['livreur']==true){
+            echo "Livreur";
+            if($client['role']['admin']==true){
+                echo "&Admin";
+            }
+            if($client['role']['restaurateur']==true){
+                echo "&Restaurateur";
+            }
+        }
+        else if(($client['role']['admin']==true)){
+            echo "Admin";
+            if($client['role']['livreur']==true){
+                echo "&Livreur";
+            }
+            if($client['role']['restaurateur']==true){
+                echo "&Restaurateur";
+            }
+        }
+        else if(($client['role']['restaurateur']==true)){
+            echo "Restaurateur";
+            if($client['role']['admin']==true){
+                echo "&Admin";
+            }
+            if($client['role']['livreur']==true){
+                echo "&Livreur";
+            }
+        }
+        else{
+            echo "Client";
+        }
     }
-    function aff_num_cmd_ou_fidelite($num, $cmd_ou_fidelite){
-        if($cmd_ou_fidelite==1){
-            if($num<10){
-                echo "000".$num;
-            }
-            else if($num<100){
-                echo "00".$num;
-            }
-            else if($num<1000){
-                echo "0".$num;
+    function aff_num_cmd_sans_echo($num){
+    if($num<10){
+        return "000".$num;
+    }
+    else if($num<100){
+        return "00".$num;
+    }
+    else if($num<1000){
+        return "0".$num;
+    }
+    else{
+        return $num;
+    }
+}
+    foreach($data as $pers){
+        if(isset($_REQUEST['bloque_'.$pers['numero_fidelite']])){
+            if($data[$pers['email']]['role']['bloque']==true){
+                $data[$pers['email']]['role']['bloque']=false;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
             }
             else{
-                echo $num;
+                $data[$pers['email']]['role']['bloque']=true;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
             }
         }
-        else{
-            if($num<10){
-                echo "0000000".$num;
-            }
-            else if($num<100){
-                echo "000000".$num;
-            }
-            else if($num<10000){
-                echo "00000".$num;
-            }
-            else if($num<100000){
-                echo "0000".$num;
-            }
-            else if($num<1000000){
-                echo "000".$num;
-            }
-            else if($num<10000000){
-                echo "00".$num;
-            }
-            else if($num<100000000){
-                echo "0".$num;
+        if(isset($_REQUEST['restaurateur_'.$pers['numero_fidelite']])){
+            if($data[$pers['email']]['role']['restaurateur']==false){
+                $data[$pers['email']]['role']['restaurateur']=true;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
             }
             else{
-                echo $num;
+                $data[$pers['email']]['role']['restaurateur']=false;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
             }
         }
-    }
-    function aff_num_cmd($num){
-        if($num<10){
-            return "000".$num;
+        if(isset($_REQUEST['admin_'.$pers['numero_fidelite']])){
+            if($data[$pers['email']]['role']['admin']==false){
+                $data[$pers['email']]['role']['admin']=true;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
+            }
+            else{
+                $data[$pers['email']]['role']['admin']=false;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
+            }
         }
-        else if($num<100){
-            return "00".$num;
+        if(isset($_REQUEST['livreur_'.$pers['numero_fidelite']])){
+            if($data[$pers['email']]['role']['livreur']==false){
+                $data[$pers['email']]['role']['livreur']=true;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
+            }
+            else{
+                $data[$pers['email']]['role']['livreur']=false;
+                file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT));
+                header("Location: admin.php");
+                exit;
+            }
         }
-        else if($num<1000){
-            return "0".$num;
-        }
-        else{
-            return $num;
-        }
-    }
-    function aff_temps($num){
-        if($num<10){
-            return "0".$num;
-        }
-        else{
-            return $num;
-        }
-    }  
-    foreach($commande[$mail] as $id_cmd => $details){
-        if(isset($_REQUEST[aff_num_cmd($details['num'])])){
-            foreach($details['plats'] as $id => $pla){
-                $panier['total'] += ($pla['quantite'] * $pla['prix']);
-                if(isset($panier['plats'][$id])){
-                    $panier['plats'][$id]['quantite'] += $pla['quantite'];
-                }
-                else{
-                    $panier['plats'][$id] = $pla;
+        if(isset($_REQUEST['supprimer_'.$pers['numero_fidelite']])){
+            $commande_data = file_get_contents("donnees/commande.json");
+            $commande = json_decode($commande_data, true);
+            $commande_passees_data = file_get_contents("donnees/commande_passe.json");
+            $commande_passees = json_decode($commande_passees_data, true);
+            if(file_exists("donnees/client_spprimes.json")){
+                $client_supp_end=json_decode(file_get_contents("donnees/client_spprimes.json"), true);
+            }
+            else{
+                $client_supp_end=[];
+            }
+            if(isset($commande_passees[$pers['email']])){
+                $client_supp=array('data'=>$pers, 'commandes_passees'=>$commande_passees[$pers['email']]);
+            }   
+            else{
+                $client_supp=array('data'=>$pers, 'commandes_passees'=>[]);
+            }
+            $client_supp_end[$pers['email']]=$client_supp;
+            unset($data[$pers['email']]);
+            unset($commande_passees[$pers['email']]);
+            foreach ($commande as $id_cmd => $detail) {
+                if($detail["mail"] == $pers['email']){
+                    unset($commande[aff_num_cmd_sans_echo($detail['num'])]);
                 }
             }
-            file_put_contents("donnees/panier_$mail.json", json_encode($panier, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-            header("Location: panier.php");
-            exit; 
+            file_put_contents("donnees/client_spprimes.json", json_encode($client_supp_end, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            file_put_contents("donnees/commande.json", json_encode($commande, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            file_put_contents("donnees/commande_passe.json", json_encode($commande_passees, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            header("Location: admin.php");
+            exit;
         }
     }
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="format-detection" content="telephone=no">
     <title>Les Croquettes du Chef</title>
     
     <link rel="stylesheet" href="css/variables.css">
-    <link rel="stylesheet" href="css/profil.css">
+    <link rel="stylesheet" href="css/admin.css">
     <link rel="stylesheet" href="css/client.css">
     <link href="assets/Logo projet.png" rel="icon">
 </head>
 <body>
-    <header>
+<header>
         <div class="logo">
             <img src="assets/Logo projet.png" alt="Logo" class="header-logo" style="height: 35px;">
-            Espace client
+            Espace Administrateur 
         </div>
         <nav>
             <ul>
-                <?php if(isset($client['role']['restaurateur']) && $client['role']['restaurateur'] == true && !isset($_COOKIE["admin"])){ ?>
-                    <li><a href="commandes.php">Commande</a></li>
-                <?php } ?>
-                <?php if(isset($client['role']['livreur']) && $client['role']['livreur'] == true && !isset($_COOKIE["admin"])){ ?>
-                    <li><a href="livraisons.php">Livraison</a></li>
-                <?php } ?>
-                <?php if(isset($client['role']['admin']) && $client['role']['admin'] == true && !isset($_COOKIE["admin"])){ ?>
-                    <li><a href="admin.php">Administration</a></li>
-                <?php } ?>
-                <?php if(!isset($_COOKIE["admin"])){ ?>
-                    <li><a href="menu.php">La Carte</a></li>
-                <?php } ?>
-                <?php if(!isset($_COOKIE["admin"])){ ?>
-                    <li><a href="panier.php">🛒</a></li>
-                <?php } ?>
-                <?php if(!isset($_COOKIE["admin"])){ ?>
-                    <li><a href="profil.php" class="active">Mon profil</a></li>
-                <?php } ?>
-                <?php if(isset($_COOKIE["admin"])){ ?>
-                    <li><a href="administration.php" class="btn">Retour page admin</a></li>
-                <?php }else{ ?>
-                    <li><a href="logout.php" class="btn">Déconnexion</a></li>
-                <?php } ?>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="menu.php">La Carte</a></li>
+                <li><a href="admin.php" class="active">Admin</a></li>
+                <li><a href="<?php if(isset($_COOKIE["client"])){ echo "profil.php"; } else{ echo "login.php"; }  ?>" class="btn">
+                        <?php  
+                            if(isset($_COOKIE["client"])){
+                                echo "Profil";
+                            }
+                            else{
+                                echo "Connexion";
+                            }
+                        ?>
+                    </a></li>
             </ul>
         </nav>
-    </header>
-    <main class="container">
-    <aside class="sidebar-perso">
-        <div class="titre-modif">
-            <h3>Mes informations</h3>
-            <a href="modification.php" class="carre-crayon">✏️</a>
-        </div>
-        <div class="info-details">
-            <p>NOM :</p>
-            <p><strong><?php echo $client['fname']; ?></strong></p>
-            <p>PRÉNOM :</p>
-            <p><strong><?php echo $client['name']; ?></strong></p>
-            <p>ADRESSE :</p>
-            <p><strong><?php echo $client['adr']; ?></strong></p>
-            <p>TÉLÉPHONE :</p>
-            <p><strong><?php echo $client['tel']; ?></strong></p>
-            <?php if($client['infocomp']!=""){ ?>
-                <p>INFOS COMPLÉMENTAIRES :</p>
-                <p><strong><?php echo $client['infocomp']; ?></strong></p>
-            <?php } ?>
-        </div>
-    </aside>
-
-    <section class="contenu-profil">
-        <h1>Mon Profil</h1>
-
-        <div class="carte-info">
-            <h3>Programme De Fidélité</h3>
-            <p>
-                N° de carte de fidélité :<br />
-                <span class="numero-carte"><?php aff_num_cmd_ou_fidelite($client['numero_fidelite'], 2); ?></span>
-            </p>
-            <?php $pourcentage=(100*$client['point_fidelite'])/300;
-                if($pourcentage>=100){
-                    $pourcentage=100;
-                }
-            ?>
-            <p>Vous avez <strong><?php  echo $client['point_fidelite']; ?> points</strong></p>
-            <div class="barre">
-                <div class="avancee" style="width:<?php echo $pourcentage; ?>%;"></div>
-            </div>
-            <?php if($pourcentage<100){ ?>
-                <p><small>Encore <?php echo 300-($client['point_fidelite']); ?> points avant la réduction de 25% sur la commande suivante !</small></p>
-            <?php }else{ ?>
-                <p><small>🎉 Félicitations ! Vous avez débloqué une réduction de 25% sur votre prochaine commande !</small></p>
-            <?php } ?>
-            
-        </div>
-        <div class="carte-info">
-            <h3>Anciennes commandes 🛍️</h3>
-                <?php 
-                    $email = $client['email'];
-                    if(!empty($commande[$email])){
-                        foreach($commande[$email] as $id_cmd => $details){ ?>
-                            <div class="commande">
-                                <div class="numero">
-                                    <strong>Commande n°<?php aff_num_cmd_ou_fidelite($details['num'], 1) ;?> (<?php echo aff_temps($details['date']['jour'])."/".aff_temps($details['date']['mois'])."/".aff_temps($details['date']['annee']).":".aff_temps($details['date']['heure']).":".aff_temps($details['date']['minute']); ?>)</strong>
-                                    <form method="POST"><button name="<?php aff_num_cmd_ou_fidelite($details['num'], 1) ; ?>" class="bouton-recommande">Recommander</button></form>
-                                </div>
-                                <?php foreach($details['plats'] as $produit){ ?>
-                                <p><?php echo $produit['quantite']."x       -".$produit['name']; ?> - <?php echo number_format($produit['quantite']*$produit['prix'], 2, ',', ' '); ?>€</p>
-                                <?php } ?>
-                            </div>
-                        <?php } 
-                    } else { ?>
-                        <div class="commande">
-                            <div class="numero">
-                                <strong><br>Vous n'avez pas encore passé de commande.</strong>
-                                <button name="<?php aff_num_cmd_ou_fidelite($details['num'], 1) ; ?>" class="bouton-recommande">Commander</button>
-                            </div>
-                        </div>
+</header>
+<section class="titre-admin">
+    <h1>Utilisateurs</h1>
+    <div class="filtres-admin">
+        <label for="filtres-choix">Filtrer :</label>
+        <select name="type-utilisateur" id="filtres-choix">
+            <option value="tous">Tous</option>
+            <option value="clients">Tous les clients</option>
+            <option value="commandes">Clients ayant déjà commandé</option>
+            <option value="livreurs">Livreurs</option>
+            <option value="administrateurs">Administrateurs</option>
+        </select>
+    </div>
+</section>
+<main>
+<table border="1">
+    <tr>
+        <th>Email</th><th>Nom</th><th>Rôle</th><th>Actions</th>
+    </tr>
+    <?php foreach($data as $pers){ ?>
+    <tr>
+        <td><?php echo $pers['email'] ?></td>
+        <td><?php echo $pers['name']." ".$pers['fname'] ?></td>
+        <td><?php aff_role($pers) ?></td>
+        <td>
+            <form method="POST" action="admin.php">
+                <?php if($pers['email']!="nathaneviname@gmail.com"){ ?>
+                    <p><a href="">Aller sur le profil</a></p>
+                    <p><button name="bloque_<?php echo $pers['numero_fidelite']; ?>"><?php if($pers['role']['bloque']==true){ echo "Débloquer";}else{ echo "Bloquer";} ?></button></p>
                 <?php } ?>
-        </div>        
-    </section>    
+                <p><button name="restaurateur_<?php echo $pers['numero_fidelite']; ?>"><?php if($pers['role']['restaurateur']==true){ echo "Retirer restaurateur";}else{ echo "Passer en restaurateur";} ?></button></p>
+                <?php if($pers['email']!="nathaneviname@gmail.com"){ ?>
+                    <p><button name="admin_<?php echo $pers['numero_fidelite']; ?>"><?php if($pers['role']['admin']==true){ echo "Retirer admin";}else{ echo "Passer en admin";} ?></button></p>
+                <?php } ?>
+                <p><button name="livreur_<?php echo $pers['numero_fidelite']; ?>"><?php if($pers['role']['livreur']==true){ echo "Retirer livreur";}else{ echo "Passer en livreur";} ?></button></p>
+                <?php if($pers['email']!="nathaneviname@gmail.com"){ ?>
+                    <p><button name="supprimer_<?php echo $pers['numero_fidelite']; ?>">Supprimer</button></p>
+                <?php } ?>
+            </form>
+        </td>
+    </tr>
+    <?php } ?>
+</table>
 </main>
 <footer>
-    <p>&copy; 2026 Les Croquettes du Chef - Espace Client</p>
-</footer> 
+    <p>&copy; 2026 Les Croquettes du Chef - Espace Pro</p>
+</footer>
 </body>
 </html>
 
